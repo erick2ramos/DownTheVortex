e3ad21c28cf4e935b1e360733d4de854d9fc0a21
Erick Dos Ramos <erick2ramos@gmail.com>
2019-09-07 12:31:54 -0400

    Input manager mouse handler. Adding LWRP and shader graph

--- DownTheVortex/Assets/01_Scripts/InputSystem/InputManager.cs
+++ DownTheVortex/Assets/01_Scripts/InputSystem/InputManager.cs
@@ -16,6 +16,8 @@ namespace BaseSystems.Input
         public event Action<TouchInputEvent> OnTouchStay;
         public event Action<TouchInputEvent> OnTouchRelease;
 
+        private Vector3 _lastMousePosition;
+
         public override void Initialize()
         {
             _eventSystem = GetComponent<EventSystem>();
@@ -23,6 +25,38 @@ namespace BaseSystems.Input
 
         private void Update()
         {
+
+#if UNITY_EDITOR
+            if (UInput.GetMouseButtonDown(0))
+            {
+                _lastMousePosition = UInput.mousePosition;
+                TouchInputEvent tie = new TouchInputEvent()
+                {
+                    touchPos = _lastMousePosition,
+                    touchDelta = Vector3.zero
+                };
+                OnTouchStart?.Invoke(tie);
+            }
+            else if (UInput.GetMouseButton(0))
+            {
+                TouchInputEvent tie = new TouchInputEvent()
+                {
+                    touchPos = UInput.mousePosition
+                };
+                tie.touchDelta = tie.touchPos - _lastMousePosition;
+                _lastMousePosition = tie.touchPos;
+                OnTouchStay?.Invoke(tie);
+            }
+            else if (UInput.GetMouseButtonUp(0))
+            {
+                TouchInputEvent tie = new TouchInputEvent()
+                {
+                    touchPos = UInput.mousePosition,
+                    touchDelta = Vector3.zero
+                };
+                OnTouchRelease?.Invoke(tie);
+            }
+#elif UNITY_ANDROID
             if (UInput.touchCount > 0)
             {
                 // Only polling touch 0
@@ -49,6 +83,7 @@ namespace BaseSystems.Input
                         break;
                 }
             }
+#endif
         }
     }
 }
