909530e49891e0b944187e04dfff71285b6db518
Erick Dos Ramos <erick2ramos@gmail.com>
2019-09-06 21:55:05 -0400

    Audio system

---
+++ DownTheVortex/Assets/01_Scripts/AudioSystem/AudioManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using BaseSystems.Managers;
+using System;
+using System.Collections.Generic;
+
+namespace BaseSystems.Audio
+{
+    public enum AudioID
+    {
+        BackgroundMusic = 0,
+        Button0 = 1,
+        Button1 = 2,
+        Death0 = 3,
+        Death1 = 4,
+        Clear0 = 5,
+        Clear1 = 6,
+        Win = 7,
+        Lose = 8
+    }
+
+    public class AudioManager : Manager
+    {
+        [Serializable]
+        class AudioTuple
+        {
+            public AudioID Id;
+            public AudioClip Sound;
+        }
+
+        public AudioSource MusicSource;
+        public AudioSource SFXSource;
+        [SerializeField]
+        List<AudioTuple> _soundMap;
+
+        public void PlayBackgroundMusic(AudioID audioId)
+        {
+            AudioTuple clip = _soundMap.Find(audio => audio.Id == audioId);
+            if(clip != null)
+            {
+                MusicSource.clip = clip.Sound;
+                MusicSource.Play();
+            } else
+            {
+                GameLog.LogError("No sound configured for id: " + audioId.ToString());
+            }
+        }
+
+        public void PlaySFX(AudioID audioId)
+        {
+            AudioTuple clip = _soundMap.Find(audio => audio.Id == audioId);
+            if (clip != null)
+            {
+                SFXSource.PlayOneShot(clip.Sound);
+            }
+            else
+            {
+                GameLog.LogError("No sound configured for id: " + audioId.ToString());
+            }
+        }
+    }
+}
---
+++ DownTheVortex/Assets/01_Scripts/AudioSystem/SoundPreferences.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BaseSystems.Audio
+{
+    [System.Serializable]
+    public class SoundPreferences
+    {
+        public float MasterVolume = 1;
+        public float SFXVolume = 1;
+        public bool IsMuted = false;
+    }
+}
