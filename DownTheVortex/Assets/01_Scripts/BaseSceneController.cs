ab844dabdfcb5c03faa43dac4645c1d83b494bec
Erick Dos Ramos <erick2ramos@gmail.com>
2019-09-06 22:36:57 -0400

    Initialization flow

---
+++ DownTheVortex/Assets/01_Scripts/BaseSceneController.cs
@@ -0,0 +1,26 @@
+﻿using BaseSystems.DataPersistance;
+using BaseSystems.Managers;
+using System.Collections;
+using UnityEngine;
+
+namespace BaseSystems.SceneHandling
+{
+    public class BaseSceneModel : SceneModel
+    {
+
+    }
+
+    public class BaseSceneController : SceneController<BaseSceneModel>
+    {
+        public override IEnumerator Initialization()
+        {
+            var DPManager = ManagerHandler.Get<DataPersistanceManager>();
+
+            DataPersistanceManager.PlayerData.CurrentHighScore++;
+            yield return new WaitForSeconds(1);
+            DPManager.Save();
+
+            GameLog.Log(DataPersistanceManager.PlayerData.CurrentHighScore);
+        }
+    }
+}
---
+++ DownTheVortex/Assets/01_Scripts/StartingFlow/InitializationHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using BaseSystems.Managers;
+using BaseSystems.SceneHandling;
+
+public class InitializationHandler : MonoBehaviour
+{
+    ManagerHandler handler;
+
+    IEnumerator Start()
+    {
+        handler = FindObjectOfType<ManagerHandler>();
+
+        handler.Init();
+
+        yield return new WaitForSeconds(1);
+
+        BaseSceneModel model = new BaseSceneModel();
+        ManagerHandler.Get<SceneTransitionManager>().LoadScene(SceneIndex.GameScene, model);
+    }
+}
