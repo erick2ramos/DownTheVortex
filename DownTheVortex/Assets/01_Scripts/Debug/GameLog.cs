4abc0c9e45bb2cd8637d53cbb9df09e2ac15c26e
Erick Dos Ramos <erick2ramos@gmail.com>
2019-09-06 21:51:39 -0400

    Adding utilities

---
+++ DownTheVortex/Assets/01_Scripts/Debug/GameLog.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameLog
+{
+    public static void Log(object message, Object context)
+    {
+        Debug.Log(message, context);
+    }
+
+    public static void Log(object message)
+    {
+        Debug.Log(message);
+    }
+
+    public static void LogError(object message)
+    {
+        Debug.LogError(message);
+    }
+    public static void LogWarning(object message)
+    {
+        Debug.LogWarning(message);
+    }
+}
