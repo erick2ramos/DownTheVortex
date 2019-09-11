aac1eb3d18f8e6c61016b0c5d86becc93ce0f131
Erick Dos Ramos <erick2ramos@gmail.com>
2019-09-10 21:22:57 -0300

    Feedback system implementation

---
+++ DownTheVortex/Assets/01_Scripts/FeedbackSystem/Feedbacks/SoundFeedback.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using BaseSystems.Audio;
+using BaseSystems.Managers;
+
+namespace BaseSystems.Feedback {
+    public class SoundFeedback : Feedback
+    {
+        public AudioID SFXID;
+
+        protected override void CustomPlay()
+        {
+            ManagerHandler.Get<AudioManager>().PlaySFX(SFXID);
+        }
+    }
+}
